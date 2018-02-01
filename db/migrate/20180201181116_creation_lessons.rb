af10c5baf9f637d74e451d672968076c92c3a4ee
TinaHelfer <34861185+TinaHelfer@users.noreply.github.com>
2018-02-01 21:28:29 +0100

    Update 20180201181116_creation_lessons.rb

--- db/migrate/20180201181116_creation_lessons.rb
+++ db/migrate/20180201181116_creation_lessons.rb
@@ -1,6 +1,6 @@
 class CreationLessons < ActiveRecord::Migration[5.1]
   def change
-    create_table :lecons do |t|
+    create_table :lessons do |t|
     t.string :title
       t.text :content
     end
